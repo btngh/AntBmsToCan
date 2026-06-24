8c5de368b54886a4b81b745904d5094e27645e01
btngh <itcenter4it@gmail.com>
2026-06-25 04:30:53 +0700

    Update wget command to download b3.sh

--- AntBmsToCan/b3.sh
+++ AntBmsToCan/b3.sh
@@ -1,6 +1,6 @@
 # apt install wget
 # rm clone_emmc.sh
-# wget -O boot.sh https://github.com/btngh/AntBmsToCan/raw/refs/heads/master/AntBmsToCan/boot.sh?v=$(date +%s)
+# wget -O b3.sh https://github.com/btngh/AntBmsToCan/raw/refs/heads/master/AntBmsToCan/b3.sh?v=$(date +%s)
 # chmod +x boot.sh
 # ./boot.sh
 
