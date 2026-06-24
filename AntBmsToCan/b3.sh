f7952b67a3d3379c50471b914eb6173e57d27d86
btngh <itcenter4it@gmail.com>
2026-06-25 04:46:06 +0700

    Update b3.sh

--- AntBmsToCan/b3.sh
+++ AntBmsToCan/b3.sh
@@ -33,7 +33,42 @@ wget https://github.com/BPI-SINOVOIP/BPI-files/raw/refs/heads/master/SD/100MB/u-
 gunzip -c BPI-R2-EMMC-boot0-DDR1600-0k-0905.img.gz | dd of=/dev/mmcblk1boot0 bs=1k seek=0 conv=notrunc
 gunzip -c u-boot-2019.07-bpi-r2-2k.img.gz | dd of=/dev/mmcblk1boot0 bs=1k seek=320 conv=notrunc
 
+mmc bootpart enable 1 1 /dev/mmcblk1
+# Clone 7.3G dữ liệu
+#dd if=/dev/mmcblk0 | pv -s 7300M | dd of=/dev/mmcblk1 bs=4M conv=fsync
+dd if=/dev/mmcblk0 bs=4M count=1825 | pv -s 7300M | dd of=/dev/mmcblk1 bs=4M conv=fsync
+
+echo "============================================="
+echo "BƯỚC 5: SỬA LỖI ĐỊNH DẠNG BLOCK BITMAP..."
+echo "============================================="
+fsck.ext4 -y /dev/mmcblk1p2
+
+echo "============================================="
+echo "BƯỚC 6: CAN THIỆP HỆ THỐNG EMMC (DÙNG ECHO ĐỂ TRÁNH LỖI EOF)..."
+echo "============================================="
+mkdir -p /mnt/emmc_boot /mnt/emmc_rootfs
+mount /dev/mmcblk1p1 /mnt/emmc_boot
+mount /dev/mmcblk1p2 /mnt/emmc_rootfs
+
+# Sửa uEnv.txt và fstab bằng lệnh echo (An toàn hơn cat <<EOL)
+echo "root=/dev/mmcblk1p2 rootwait" > /mnt/emmc_boot/bananapi/bpi-r2/linux/uEnv.txt
+echo -e "/dev/mmcblk1p1 /boot vfat errors=remount-ro 0 1\n/dev/mmcblk1p2 / ext4 defaults 0 0" > /mnt/emmc_rootfs/etc/fstab
+
+# Chặn driver đồ họa và các dịch vụ gây treo máy 90s
+#echo "blacklist lima" > /mnt/emmc_rootfs/etc/modprobe.d/blacklist.conf
+#ln -sf /dev/null /mnt/emmc_rootfs/etc/systemd/system/systemd-journald.service
+#ln -sf /dev/null /mnt/emmc_rootfs/etc/systemd/system/NetworkManager-wait-online.service
+#rm -f /mnt/emmc_rootfs/lib/systemd/system/systemd-journal-flush.service
+#rm -rf /mnt/emmc_rootfs/var/log/journal/*
+#echo "kernel.printk = 3 4 1 3" >> /mnt/emmc_rootfs/etc/sysctl.conf
+
+echo "============================================="
+echo "BƯỚC 7: HOÀN TẤT VÀ TỰ HỦY SCRIPT TRÊN THẺ SD..."
+echo "============================================="
+umount /mnt/emmc_boot /mnt/emmc_rootfs
 sync
+# Lệnh tự xóa chính nó trên thẻ SD
+rm -f "$0"
 echo "XONG! Rút thẻ SD và máy sẽ tự tắt sau 5s."
 sleep 5
 poweroff
