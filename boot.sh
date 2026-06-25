9e7babc17a31a3b4fb3a0566b914656edb437eb1
btngh <itcenter4it@gmail.com>
2026-06-25 23:53:29 +0700

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -16,12 +16,18 @@ echo "NẠP PRELOADER GỐC EMMC 2019 (FIX LỖI LỆNH TẢI)..."
 echo "============================================="
 echo 0 > /sys/block/mmcblk1boot0/force_ro
 # Đã thêm lệnh wget và dùng link Raw chuẩn
-# wget https://github.com/BPI-SINOVOIP/BPI-files/raw/refs/heads/master/SD/100MB/BPI-R2-EMMC-boot0-DDR1600-20190722-0k.img.gz 
-wget https://github.com/BPI-SINOVOIP/BPI-files/raw/refs/heads/master/SD/100MB/BPI-R2-EMMC-boot0-DDR1600-0k-0905.img.gz
-# gunzip -c BPI-R2-EMMC-boot0-DDR1600-20190722-0k.img | dd of=/dev/mmcblk1boot0 bs=1024 seek=0
-gunzip -c BPI-R2-EMMC-boot0-DDR1600-0k-0905.img.gz | dd of=/dev/mmcblk1boot0 bs=1024 seek=0
+wget https://github.com/BPI-SINOVOIP/BPI-files/raw/refs/heads/master/SD/100MB/BPI-R2-EMMC-boot0-DDR1600-20190722-0k.img.gz 
+
+wget https://github.com/BPI-SINOVOIP/BPI-files/raw/refs/heads/master/SD/100MB/u-boot-2019.07-bpi-r2-2k.img.gz
+
+gunzip -c BPI-R2-EMMC-boot0-DDR1600-20190722-0k.img.gz  | dd of=/dev/mmcblk1boot0 bs=1024 seek=0
+
 sudo mmc bootpart enable 1 1 /dev/mmcblk1
 echo 1 > /sys/block/mmcblk1boot0/force_ro
+
+gunzip -c /u-boot-2019.07-bpi-r2-2k.img.gz | dd of=/dev/mmcblk1 bs=512 seek=640 conv=notrunc
+# of=/dev/mmcblk1 bs=512 seek=640 conv=notrunc
+# of=/dev/mmcblk1 bs=1k seek=320 conv=notrunc
 
 sync
 # Lệnh tự xóa chính nó trên thẻ SD
