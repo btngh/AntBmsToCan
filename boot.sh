46ecfb56a0de35a50f32fd40391814e819a2deb1
btngh <itcenter4it@gmail.com>
2026-06-26 01:01:07 +0700

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -23,20 +23,24 @@ systemctl restart systemd-timesyncd || true
 # =========================================================================
 
 
-echo "NẠP PRELOADER GỐC EMMC 2019 (FIX LỖI LỆNH TẢI)..."
+
+
+
+
+echo "NẠP boot0 GỐC EMMC 2019 (FIX LỖI LỆNH TẢI)..."
 echo "============================================="
 echo 0 > /sys/block/mmcblk1boot0/force_ro
 # Đã thêm lệnh wget và dùng link Raw chuẩn
-wget -O preloader.img.gz https://github.com/BPI-SINOVOIP/BPI-files/raw/refs/heads/master/SD/100MB/BPI-R2-preloader-DDR1600-20191024-2k.img.gz?v=$(date +%s)
+wget -O boot0.img.gz https://github.com/BPI-SINOVOIP/BPI-files/raw/refs/heads/master/SD/100MB/BPI-R2-EMMC-boot0-DDR1600-20191024-0k.img.gz?v=$(date +%s)
 dd if=/dev/zero of=/dev/mmcblk1boot0 bs=1k count=1024 conv=notrunc
-gunzip -c preloader.img.gz  | dd of=/dev/mmcblk1boot0 bs=1024 seek=0
+gunzip -c boot0.img.gz  | dd of=/dev/mmcblk1boot0 bs=1024 seek=0
 echo 1 > /sys/block/mmcblk1boot0/force_ro
 
 
 
-wget -O uboot.img.gz https://github.com/BPI-SINOVOIP/BPI-files/raw/refs/heads/master/SD/100MB/u-boot-2019.07-bpi-r2-2k.img.gz?v=$(date +%s)
-gunzip -c uboot.img.gz | dd of=/dev/mmcblk1 bs=1k seek=2 count=1022
-sudo mmc bootpart enable 1 1 /dev/mmcblk1
+#wget -O uboot.img.gz https://github.com/BPI-SINOVOIP/BPI-files/raw/refs/heads/master/SD/100MB/u-boot-2019.07-bpi-r2-2k.img.gz?v=$(date +%s)
+#gunzip -c uboot.img.gz | dd of=/dev/mmcblk1 bs=1k seek=2 count=1022
+#sudo mmc bootpart enable 1 1 /dev/mmcblk1
 
 sync
 # Lệnh tự xóa chính nó trên thẻ SD
