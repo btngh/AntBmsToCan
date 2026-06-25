7791b161602e36aca5c5a0c040db8b94f3958956
btngh <itcenter4it@gmail.com>
2026-06-25 14:04:35 +0700

    Update qq.sh

--- qq.sh
+++ qq.sh
@@ -36,7 +36,8 @@ rm -f preloader_emmc.img
 echo "============================================="
 echo "BƯỚC 3: ĐỒNG BỘ U-BOOT SANG BỘ NHỚ EMMC CHÍNH..."
 echo "============================================="
-dd if=/dev/mmcblk0 of=/dev/mmcblk1 bs=1k skip=320 seek=320 count=1024 conv=sync,notrunc
+
+dd if=/dev/mmcblk0 of=/dev/mmcblk1 bs=1K skip=320 seek=320 count=1024 conv=notrunc && sync
 
 mmc bootpart enable 1 1 /dev/mmcblk1
 
