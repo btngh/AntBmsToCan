9ec474bb395194744f8a542438698af538e59e78
btngh <itcenter4it@gmail.com>
2026-06-25 10:07:49 +0700

    Update b3.sh

--- AntBmsToCan/b3.sh
+++ AntBmsToCan/b3.sh
@@ -37,17 +37,17 @@ echo "============================================="
 echo 0 > /sys/block/mmcblk1boot0/force_ro
 # Đã thêm lệnh wget và dùng link Raw chuẩn
 wget https://github.com/BPI-SINOVOIP/BPI-files/raw/refs/heads/master/SD/100MB/BPI-R2-EMMC-boot0-DDR1600-20190722-0k.img.gz
-wget https://github.com/BPI-SINOVOIP/BPI-files/raw/refs/heads/master/SD/100MB/u-boot-2019.07-bpi-r2-2k.img.gz
+#wget https://github.com/BPI-SINOVOIP/BPI-files/raw/refs/heads/master/SD/100MB/u-boot-2019.07-bpi-r2-2k.img.gz
 gunzip -c BPI-R2-EMMC-boot0-DDR1600-20190722-0k.img.gz | dd of=/dev/mmcblk1boot0 bs=1k seek=0 conv=notrunc
 
 
 mmc bootpart enable 1 1 /dev/mmcblk1
 echo 1 > /sys/block/mmcblk1boot0/force_ro
 # 2. Ghi hệ điều hành (ổ đĩa chính)
-dd if=/dev/mmcblk0 of=/dev/mmcblk1 bs=4M conv=fsync # Sử dụng 1 lệnh duy nhất
-partprobe /dev/mmcblk1
-gunzip -c u-boot-2019.07-bpi-r2-2k.img.gz | dd of=/dev/mmcblk1 bs=1k seek=320 conv=notrunc
-sync
+dd if=/dev/mmcblk0 | pv -s 7300M | dd of=/dev/mmcblk1 bs=4M conv=fsync
+
+# gunzip -c u-boot-2019.07-bpi-r2-2k.img.gz | dd of=/dev/mmcblk1 bs=1k seek=320 conv=notrunc
+
 echo "============================================="
 echo "BƯỚC 5: SỬA LỖI ĐỊNH DẠNG BLOCK BITMAP..."
 echo "============================================="
